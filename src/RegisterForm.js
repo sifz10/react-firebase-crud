1245cae57e8c0f5a54bdd3eb7e780ea88598daec
SIFAT69 <sifatkazi69@gmail.com>
2022-09-28 03:57:46 +0600

    Reading the input

--- src/RegisterForm.js
+++ src/RegisterForm.js
@@ -1,12 +1,16 @@
-import {Container, Row, Col, Button, Form} from 'react-bootstrap';
-import React from 'react'
+import {useState}  from 'react';
+import {Button, Form} from 'react-bootstrap';
 
 function RegisterForm() {
+    let [batchName, setBatchname] = useState("");
+    let handleBatchName = (e) => {
+        setBatchname(e.target.value);
+    };
   return (
         <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Batch Number</Form.Label>
-                <Form.Control type="text" placeholder="Enter Batch Number" />
+                <Form.Control type="text" onChange={handleBatchName}  placeholder="Enter Batch Number" />
                 <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
                 </Form.Text>
@@ -20,19 +24,19 @@ function RegisterForm() {
                 <Form.Label>Class Topic</Form.Label>
                 <Form.Control type="text" placeholder="Class Topic" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Group className="mb-3" controlId="formBasicCheckbox1">
                 <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Group className="mb-3" controlId="formBasicCheckbox2">
                 <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Group className="mb-3" controlId="formBasicCheckbox3">
                 <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Group className="mb-3" controlId="formBasicCheckbox4">
                 <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Group className="mb-3" controlId="formBasicCheckbox5">
                 <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
             <Button variant="primary" type="submit">
